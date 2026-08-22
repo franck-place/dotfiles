51dac12622f5299fee381079404f850ea2e79711
Franck <francois.laflamme99@proton.me>
2026-08-22 13:20:03 -0400

    Delete .config directory

--- .config/qtile/scripts/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-picom --config ~/.config/qtile/scripts/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/bin/wired &
-eval $(gnome-keyring-daemon --start) 
-nm-applet &
-vesktop &
-firefox-developer-edition &
-flameshot &
